e706babbe6cac7f4e2f2d2d3280cb93112592d50
Matthew Gilligan <mcgilligan97@gmail.com>
2022-06-24 18:05:48 +0100

    Play around with project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,16 +7,66 @@ import './Projects.css';
 export default function Projects() {
   const [projects, setProjects] = createSignal(ProjectsData);
 
+  const handleProjectClick = (e) => {
+    const alreadyActive = e.target.parentNode.classList.contains('active');
+    let anyElementActive = false;
+
+    const elements = document.getElementsByClassName('project');
+
+    for (const element of elements) {
+      if (element.classList.contains('active')) {
+        anyElementActive = true;
+      }
+    }
+
+    console.log(alreadyActive);
+    console.log(anyElementActive);
+
+    for (const element of elements) {
+      if (anyElementActive && !alreadyActive) {
+        element.classList.remove('active');
+        element.classList.add('inactive');
+      } else if (anyElementActive && alreadyActive) {
+        element.classList.remove('active');
+        element.classList.remove('inactive');
+      } else {
+        element.classList.add('inactive');
+      }
+    }
+
+    if (alreadyActive) {
+      e.target.parentNode.classList.remove('active');
+    } else {
+      e.target.parentNode.classList.add('active');
+      e.target.parentNode.classList.remove('inactive');
+    }
+  };
+
   return (
     <div class='projects'>
       <For each={projects()}>
         {(project, i) => (
           <div class='project'>
-            <img src={project.image} alt='' class='project-image' />
-            <p class='project-detail'>
+            <img
+              src={project.image}
+              alt=''
+              class='project-image'
+              onClick={(e) => handleProjectClick(e)}
+            />
+            <p class='project-details'>
               <span>{project.title}</span>, {project.label},{' '}
               {project.date.substring(project.date.length - 4)}
             </p>
+            <div class='active-project-details'>
+              <h3>{project.title}</h3>
+              <p>{project.label}</p>
+              <p>{project.date}</p>
+              <div className='streaming'>
+                <a href='#'>Spotify</a>
+                <a href='#'>Apple Music</a>
+                <a href='#'>Bandcamp</a>
+              </div>
+            </div>
           </div>
         )}
       </For>

beb02684d82d4f406f061a84eb0498299972ae64
Matthew Gilligan <matthew@soundonlive.com>
2025-06-04 11:19:30 +0900

    Update projects and some events

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,2 +1,5 @@
 export { default as Fade } from './fade.webp';
 export { default as Wrestle } from './wrestle.webp';
+export { default as Dubs } from './dubs.webp';
+export { default as BE } from './be.webp';
+export { default as Shaker } from './shaker.webp';
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,4 @@
-import { Fade, Wrestle } from './../assets/projects';
+import { Fade, Wrestle, Dubs, Shaker, BE } from './../assets/projects';
 
 export const ProjectsData = [
   {
@@ -20,28 +20,28 @@ export const ProjectsData = [
     bandcamp: 'bandcamp',
   },
   {
-    title: 'Wrestle EP',
-    image: Wrestle,
-    date: '29 November 2021',
-    label: 'Fly By Night',
+    title: 'INGI Dubs 001',
+    image: Dubs,
+    date: '4 November 2022',
+    label: 'Self-released',
     description: 'description',
     spotify: 'spotify',
     bandcamp: 'bandcamp',
   },
   {
-    title: 'Wrestle EP',
-    image: Wrestle,
-    date: '29 November 2021',
-    label: 'Fly By Night',
-    description: 'description',
-    spotify: 'spotify',
-    bandcamp: 'bandcamp',
-  },
-  {
-    title: 'Fade EP',
-    image: Fade,
-    date: '28 January 2022',
+    title: 'Shaker EP',
+    image: Shaker,
+    date: '1 March 2024',
     label: 'Scuffed',
+    description: 'description',
+    spotify: 'spotify',
+    bandcamp: 'bandcamp',
+  },
+  {
+    title: 'B.E',
+    image: BE,
+    date: '29 April 2025',
+    label: 'Elastics',
     description: 'description',
     spotify: 'spotify',
     bandcamp: 'bandcamp',
